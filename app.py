91c550b2c928b87940f04e30177bbee5d686e505
Sam Machin <smachin@Sams-iMac.local>
2017-03-06 10:08:29 +0000

    fix indentation

--- app.py
+++ app.py
@@ -5,37 +5,37 @@ import tornado.web
 
 
 class MainHandler(tornado.web.RequestHandler):
-	@tornado.web.asynchronous
-	def get(self):
+    @tornado.web.asynchronous
+    def get(self):
         name = os.getenv('NAME')
         lvn = os.getenv('MY_LVN')
-		self.content_type = 'text/plain'
-		self.write("$name is {} \n $lvn is {}".format(name, lvn))
-		self.finish()
+        self.content_type = 'text/plain'
+        self.write("$name is {} \n $lvn is {}".format(name, lvn))
+        self.finish()
 		
 class EnvHandler(tornado.web.RequestHandler):
-	@tornado.web.asynchronous
-	def get(self):
+    @tornado.web.asynchronous
+    def get(self):
         with open('.env') as f:
             data = f.read()
-		self.content_type = 'text/plain'
-		self.write(data)
-		self.finish()
+        self.content_type = 'text/plain'
+        self.write(data)
+        self.finish()
 						
 def main():
-	static_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-	print static_path
-	application = tornado.web.Application([(r"/", MainHandler),
+    static_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    print static_path
+    application = tornado.web.Application([(r"/", MainHandler),
                                             (r"/env", EnvHandler),
 											(r'/static/(.*)', tornado.web.StaticFileHandler, {'path': static_path}),
 											])
-	http_server = tornado.httpserver.HTTPServer(application)
-	port = int(os.environ.get("PORT", 5000))
-	http_server.listen(port)
-	tornado.ioloop.IOLoop.instance().start()
+    http_server = tornado.httpserver.HTTPServer(application)
+    port = int(os.environ.get("PORT", 5000))
+    http_server.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
-	main()
+    main()
 	
 	
 
